03e47096b5210577eacac715e176b0ae59efdb72
Samuel Eras <sa.eras@gmx.de>
2023-10-23 16:20:05 +0200

    create ejs views and partials

--- app.js
+++ app.js
@@ -14,35 +14,35 @@ app.listen(3000);
 
 // home page for everyone to see, with everyones recipes
 app.get('/', (req, res) => {
-    res.sendFile('./views/home.html', { root: __dirname });
+    res.render('index', { title: 'Home' });
 });
 
 // informatin of how the site works
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
 // login
 app.get('/login', (req, res) => {
-    res.sendFile('./views/login.html', { root: __dirname });
+    res.render('login', { title: 'Login/SignUp' });
 });
 
 // create a new recipes (only for logged in users)
 app.get('/create', (req, res) => {
-    res.sendFile('./views/create.html', { root: __dirname });
+    res.render('create', { title: 'Create Recipe' });
 });
 
 // list own recipes (only for logged in users)
 app.get('/my-recipes', (req, res) => {
-    res.sendFile('./views/my-recipes.html', { root: __dirname });
+    res.render('my-recipes', { title: 'My Recipes' });
 });
 
 // list recipes of other people that you saved/liked
 /* app.get('/saved', (req, res) => {
-    res.sendFile('./views/saved.html', { root: __dirname });
+    res.render('saved', { title: 'Saved Recipes' });
 }); */
 
 // errorpage
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: 'Error - 404' });
 });
