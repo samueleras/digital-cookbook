8c442eb63c3e4baba26dc3a5b9635745314618e3
Samuel Eras <sa.eras@gmx.de>
2023-10-27 18:18:30 +0200

    fix import and make it not async

--- modules/jwtAuth.js
+++ modules/jwtAuth.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken');
 const jwtEncryptionKey = require('./jwtEncryptionKey.js');
-const User = require('./models/user')
+const User = require('../DBmodels/user')
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
 
     try {
         const token = req.cookies.token;
         const userFromToken = jwt.verify(token, jwtEncryptionKey);       
-        const user = await User.findById(userFromToken.userid);
-        req.user = { userid: user._id, username: user.username, saved_recipes: user.saved_recipes};
-        next();
+        User.findById(userFromToken.userid).then((user) => {
+            req.user = { userid: user._id, username: user.username, saved_recipes: user.saved_recipes};
+            next();
+        }).catch((err) => console.log(err));
     } catch {
         res.clearCookie("token");
+        next();
 /*         res.redirect("/login"); */
     }
 
